b14d09e2e5f985a7697c5c4149cc1c6ea321d1c2
thomaszhou63 <thomaszhou63@163.com>
2018-04-08 19:59:27 +0800

    medium:three method

--- Array/No442-Find_All_Duplicates_in_an_Array.js
+++ Array/No442-Find_All_Duplicates_in_an_Array.js
@@ -6,7 +6,12 @@
  * 方法4
  */
 
+
 let arr = [4,3,2,7,8,2,3,1];
+
+/**
+ * method 1
+ */
 var findDuplicates1 = function(arr) {
   let res = [];
   arr.forEach((elem, i) => {
@@ -19,13 +24,32 @@ var findDuplicates1 = function(arr) {
   return res;
 };
 
+/**
+ *  method 2 (bad)
+ */
 var findDuplicates2 = function(arr) {
   let res = [];
   arr.forEach((elem, i) => {
     if (arr.lastIndexOf(elem) !== i) {
       res.push(elem);
     }
-  })
+  });
   return res;
-}
-console.log(findDuplicates(arr));
+};
+
+/**
+ *  method 3 (best)
+ */
+var findDuplicates3 = function(arr) {
+  let res = [],
+    len = arr.length;
+  for (let i = 0; i < len; ++i) {
+    arr[(arr[i] - 1) % len] += len;// // 对len取余，防止越界
+  }
+  for (let i = 0; i < len; ++i) {
+    if (arr[i] > 2 * len) res.push(i + 1);
+  }
+  return res;
+};
+
+console.log(findDuplicates3(arr));
